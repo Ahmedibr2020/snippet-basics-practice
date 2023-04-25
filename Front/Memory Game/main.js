f5dd445f9d196dba55b6d18504ffa07a46e29628
Ahmed Ibrahim Ali <ahmed.ali.ms@alexu.edu.eg>
2023-04-25 10:01:26 +0200

    /* Add Order Property to Game blocks */
    Add Order Property to Game blocks  from js file and generate this random order range array

--- Front/Memory Game/main.js
+++ Front/Memory Game/main.js
@@ -4,9 +4,31 @@ document.querySelector(".control-buttons span").onclick= function() {
     // console.log(urName);
     if (urName == null || urName == "") {
         document.querySelector(".name span").innerHTML = 'Unknown';
-    }else {
+    } else {
+        // Set Username
         document.querySelector(".name span").innerHTML = urName;
         
     }
+    // Remove Splash Screen
     document.querySelector(".control-buttons").remove();
 }
+/* START THE GAME */
+
+let duration = 1000;
+let blocksContainer = document.querySelector(".memory-game-blocks");
+let blocks = Array.from(blocksContainer.children);
+
+/* We used ... "split operator to use the keys directory not the iterator or Index" */
+// let orderRange = [...Array(blocks.length).keys()];
+// Another Way to get order Range
+let orderRange = Array.from(Array(blocks.length).keys());
+
+// console.log(orderRange);
+
+let testOrderRange = [5, 15, 23, 6, 9, 8, 1, 4, 2, 7, 29, 3, 21, 26, 17, 14, 22, 10, 12, 21, 13, 16, 24, 25, 18, 20, 19, 0, 28, 27];
+
+
+/* Add Order Property to Game blocks */
+blocks.forEach((block, index) => {
+    block.style.order = testOrderRange[index];
+});
